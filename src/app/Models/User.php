079ad6a78ef290e58f8610e593655d4c1d7853db
kikuuchi <>
2025-02-22 20:36:33 +0900

    fix: モデルクラス User の修正

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,47 +2,37 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // テーブル名
+    const TABLE = 'user';
+    public $table = self::TABLE;
+
+    // カラム名
+    const ID = 'id';
+    const UID = 'uid';
+    const EMAIL = 'email';
+    const PROVIDER = 'provider';
+    const TOKEN_EXPIRED_AT = 'token_expired_at';
+    const LAST_LOGINED_AT = 'last_logined_at';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    const DELETED_AT = 'deleted_at';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        self::ID,
+        self::UID,
+        self::EMAIL,
+        self::PROVIDER,
+        self::TOKEN_EXPIRED_AT,
+        self::LAST_LOGINED_AT,
+        self::CREATED_AT,
+        self::UPDATED_AT,
+        self::DELETED_AT,
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
