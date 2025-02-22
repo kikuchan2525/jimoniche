21ac33224be153dd90a62e7b8f3c3bb7abf41b1f
kikuuchi <>
2025-02-23 02:05:04 +0900

    feat: api route 設定処理の実装

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NicheSpotController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::put('user', UserController::class);
 Route::patch('login', [AuthController::class, 'login']);
 Route::get('me', [AuthController::class, 'me']);
 Route::delete('logout', [AuthController::class, 'logout']);
+Route::apiResource('niche_spot', NicheSpotController::class, ['only' => ['index', 'show']]);
