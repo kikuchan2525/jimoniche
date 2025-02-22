3b0ed5161100e94cd6110784fd1fc309e86a19fe
kikuuchi <>
2025-02-22 20:21:21 +0900

    feat: stamp テーブルを生成するマイグレーションファイルの実装

---
+++ src/database/migrations/2025_02_22_105038_create_stamp_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stamp', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->foreignId('niche_spot_id')->constrained('niche_spot')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stamp');
+    }
+};
