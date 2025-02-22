614821face581fb9ed114f3a384d93ff3c70bccb
kikuuchi <>
2025-02-22 23:37:38 +0900

    feat: api route の設定処理の実装

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::put('user', UserController::class);
+Route::patch('login', [AuthController::class, 'login']);
