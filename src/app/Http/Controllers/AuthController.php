31283a3397326df272070f429021ecc5e441bba8
kikuuchi <>
2025-02-22 23:37:24 +0900

    feat: ログイン処理を呼び出す処理の実装

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\AuthService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * AuthController コンストラクタ
+     * AuthService の依存性を注入する
+     *
+     * @param AuthService $authService
+     */
+    public function __construct(protected AuthService $authService)
+    {
+    }
+
+    /**
+     * ログインAPI
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        return $this->authService->login($request);
+    }
+}
